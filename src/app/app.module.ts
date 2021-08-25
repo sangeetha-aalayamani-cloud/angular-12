73db6908614ca55ff4ae8ef473c1a68b1a8951a4
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-25 21:57:04 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
-
+import { HttpClientModule } from '@angular/common/http';
+import { CounterComponent } from './counter/counter.component';
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//all components 
+    AppComponent, CounterComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule, FormsModule,  HttpClientModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

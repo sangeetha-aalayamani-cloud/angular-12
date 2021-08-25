593b39751899fb8934d8ecb06be10d54eb39c4b5
sangeetha-aalayamani-cloud <65112989+sangeetha-aalayamani-cloud@users.noreply.github.com>
2021-08-25 21:57:54 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
 import { Component } from '@angular/core';
-import { CheckboxControlValueAccessor } from '@angular/forms';
 import { AppService } from './app.service';
 
-@Component({
+@Component({  //decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent {//public , controller
   title = 'app1';
-  firstName = 'sange';
-  constructor(private appService: AppService){
+  userform ={firstname: 'sange', dob:''}; //model
+  users:any = [];
+  constructor(private appService: AppService) {
 
   }
   handleClick() {
-    this.appService.save();
-    console.log(this.firstName);
+    this.appService.save(this.userform,
+       (response: any)=> {//success handler
+        console.log(response);
+        this.users.push(response);
+      },
+      function (error: any) {//error handler
+        alert(error);
+      });
   }
-
-  delete() {
-    console.log("deleted")
+  deleteUser(userId:number, index:number) {
+    this.appService.deleteUser(userId,
+     ( response:any) => {
+        this.users.splice(index, 1)
+      });
+    console.log('deleted');
   }
-  
 }
